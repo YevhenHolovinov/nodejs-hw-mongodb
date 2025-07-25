80ace34b4e60e8f82392c02a32c5d7c85dd3531f
unknown <golovinov817@gmail.com>
2025-07-25 12:36:21 +0200

    feat validID error

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,9 @@
+import createHttpError from 'http-errors';
+import { isValidObjectId } from 'mongoose';
+
+export function isValidId(req, res, next) {
+  if (isValidObjectId(req.params.id) !== true) {
+    return next(new createHttpError.BadRequest('ID is not valid'));
+  }
+  next();
+}
--- src/routes/contacts.routes.js
+++ src/routes/contacts.routes.js
@@ -7,17 +7,18 @@ import {
   updateContactController,
 } from '../controllers/contacts.controllers.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getContactsControllers));
 
-router.get('/:id', ctrlWrapper(getContactControllers));
+router.get('/:id', isValidId, ctrlWrapper(getContactControllers));
 
 router.post('/', ctrlWrapper(createContactsController));
 
-router.delete('/:id', ctrlWrapper(deleteContactController));
+router.delete('/:id', isValidId, ctrlWrapper(deleteContactController));
 
-router.patch('/:id', ctrlWrapper(updateContactController));
+router.patch('/:id', isValidId, ctrlWrapper(updateContactController));
 
 export default router;
